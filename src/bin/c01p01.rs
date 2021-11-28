6cd08b02ccd3fd2c1445d0ac3f0bd54ecf4a9a75
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-11-28 22:24:23 +0100

    Setup: structure

---
+++ src/bin/c01p01.rs
@@ -0,0 +1,23 @@
+fn all_chars_unique_part_a(s: &str) -> bool {
+    true
+}
+
+fn all_chars_unique_part_b(s: &str) -> bool {
+    false
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_a() {}
+
+    #[test]
+    fn test_part_b() {}
+}
+
+fn main() {
+    all_chars_unique_part_a(&String::from("Hello world"));
+    all_chars_unique_part_b(&String::from("Hello world"));
+}
