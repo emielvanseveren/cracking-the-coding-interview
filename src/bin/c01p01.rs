428e461315a77d5cb709755ec3bb5fc9097eec1f
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-11-29 15:30:37 +0100

    Solution c01p01.rs

--- src/bin/c01p01.rs
+++ src/bin/c01p01.rs
@@ -4,12 +4,41 @@
  * What if you cannot use additional data structures?
 */
 
+// It would be even better if we used a BitSet here.
+// Unfortunately BitSet is not part of the std library at this moment in time.
 fn all_chars_unique_part_a(s: &str) -> bool {
+    use std::collections::HashSet;
+    let mut chars: HashSet<char> = HashSet::with_capacity(s.len());
+
+    for c in s.chars() {
+        if chars.contains(&c) {
+            return false;
+        }
+        chars.insert(c);
+    }
     true
 }
 
+// No additional data structures
+// Order String (O(n*log(n))
+// Create a bitset from an integer.
 fn all_chars_unique_part_b(s: &str) -> bool {
-    false
+    let mut bit_set: i64 = 0;
+    let a_int_char: i16 = 'a' as i16;
+
+    for c in s.chars() {
+        let mut int_char: i16 = c as i16;
+
+        // if char is a it becomes (1), other characters are moved 'a' positions.
+        int_char -= a_int_char;
+
+        // check if bit from certain character is already set.
+        if (1 << int_char) & bit_set != 0 {
+            return false;
+        }
+        bit_set |= 1 << int_char;
+    }
+    true
 }
 
 #[cfg(test)]
@@ -17,10 +46,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part_a() {}
+    fn test_part_a() {
+        assert!(all_chars_unique_part_a("abcdefg"));
+        assert!(!all_chars_unique_part_a("abcdefgabc"));
+    }
 
     #[test]
-    fn test_part_b() {}
+    fn test_part_b() {
+        assert!(all_chars_unique_part_b("abcdefg"));
+        assert!(!all_chars_unique_part_b("abcdefgabc"));
+    }
 }
 
 fn main() {
