93e755a3b1023bfd1c7ab3a9cdf1587dd5b6a0e5
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-11-29 22:55:01 +0100

    Add structure for 2 more problems

---
+++ src/bin/c02p01.rs
@@ -0,0 +1,23 @@
+/*
+ * PROBLEM:
+ * Remove Dups: Write code to remove duplicates from an unsorted linked list.
+ * FOLLOW UP
+ * How would you solve this problem if a temporary buffer is not allowed?
+*/
+
+fn test_remove_dups_part_a() {}
+
+fn test_remove_dups_part_b() {}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_remove_dups_part_a() {}
+
+    #[test]
+    fn test_remove_dups_part_b() {}
+}
---
+++ src/bin/c02p02.rs
@@ -0,0 +1,17 @@
+/*
+ * PROBLEM:
+ * Return Kth to Last: Implement an algorithm to find the kth to last element of a single linked
+ * list.
+*/
+
+fn kth_to_last() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_kth_to_last() {}
+}
+
+fn main() {}
