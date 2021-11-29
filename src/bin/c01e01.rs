cca1ff5ed21e33a7b6ed5457d641a615af9df7ee
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-11-29 15:30:20 +0100

    Solution c01e01.rs

--- src/bin/c01e01.rs
+++ src/bin/c01e01.rs
@@ -5,8 +5,35 @@
  *
 */
 
+// We can use the Rabin-Karp Substring Search Algorithm to solve this problem.
 fn substring(str: &str, substr: &str) -> i8 {
-    0
+    use std::collections::hash_map::DefaultHasher;
+    use std::convert::TryFrom;
+    use std::hash::{Hash, Hasher};
+
+    if substr.len() > str.len() {
+        return -1;
+    }
+
+    let mut hasher = DefaultHasher::new();
+    substr.hash(&mut hasher);
+    let substr_hash = hasher.finish();
+
+    for (i, _) in str.chars().enumerate() {
+        if i < str.len() - substr.len() + 1 {
+            let mut hasher = DefaultHasher::new();
+            str.chars()
+                .skip(i)
+                .take(substr.len())
+                .collect::<String>()
+                .hash(&mut hasher);
+
+            if hasher.finish() == substr_hash {
+                return i8::try_from(i).unwrap();
+            }
+        }
+    }
+    -1
 }
 
 #[cfg(test)]
@@ -30,7 +57,7 @@ mod tests {
 
     #[test]
     fn test_substring_end() {
-        assert_eq!(substring("hello", "el"), 3);
+        assert_eq!(substring("hello", "lo"), 3);
     }
 
     #[test]
