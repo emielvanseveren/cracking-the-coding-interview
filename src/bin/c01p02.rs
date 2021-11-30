c002632b8f89b9507271e70b6d10ffc24945baca
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-11-30 12:09:12 +0100

    Add solution for c01p02

--- src/bin/c01p02.rs
+++ src/bin/c01p02.rs
@@ -4,10 +4,24 @@
  * other.
 */
 
+// We could order the letters of the string and compare them O(n*lg(n)).
+// If the string are equal they are permutations.
 fn is_permutation(s1: &str, s2: &str) -> bool {
-    todo!()
+    let mut s1 = s1.chars().collect::<Vec<_>>();
+    s1.sort_unstable();
+
+    let mut s2 = s2.chars().collect::<Vec<_>>();
+    s2.sort_unstable();
+
+    if s1 == s2 {
+        return true;
+    }
+    false
 }
 
+// Another implementation could be creating 2 sets, and intersect them.
+// If the intersection is empty, we can consider a permutation.
+// No clue what the O is for the intersect.
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -16,6 +30,7 @@ mod tests {
     fn test_is_permutation() {
         assert!(is_permutation("abc", "cab"));
         assert!(!is_permutation("abc", "abd"));
+        assert!(!is_permutation("abcd", "abc"));
     }
 }
 
